d5654d51d212f274ab4f89428925c31aeb9b3ad9
Fajar Ramdani A <greatofjar@gmail.com>
2017-04-08 23:01:06 +0700

    1.0

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>WELCOME</title>
+</head>
+<body>
+<h2>Halaman Login</h2>
+
+<form action="log.php?op=in" method="POST">
+	User ID :
+	<input type="text" name="userid"><br>
+	Password :
+	<input type="password" name="psw"><br>
+	<input type="submit" value="login"><br>
+</form>
+	
+</body>
+</html>

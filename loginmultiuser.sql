66517743f3c59272fd9ee98ccc014ee809f029b6
Fajar Ramdani A <greatofjar@gmail.com>
2017-04-09 00:19:23 +0700

    Database

---
+++ loginmultiuser.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 08 Apr 2017 pada 18.03
+-- Versi Server: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `loginmultiuser`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tabeluser`
+--
+
+CREATE TABLE `tabeluser` (
+  `userid` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `level` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tabeluser`
+--
+
+INSERT INTO `tabeluser` (`userid`, `password`, `level`) VALUES
+('babeh', '123456', 'user'),
+('fajar', '123456', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tabeluser`
+--
+ALTER TABLE `tabeluser`
+  ADD PRIMARY KEY (`userid`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
